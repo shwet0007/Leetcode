159996e1db98e42dcaa8edc9b2393ae5bc661861
shwet0007 <shwetramteke35@gmail.com>
2026-07-31 22:19:27 +0530

    Time: 0 ms (100%), Space: 43.2 MB (47.1%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,18 +1,19 @@
 class Solution {
     public int minimumPushes(String word) {
+        
         int sum=0;
-        for(int i=0;i<word.length();i++){
-            if(i<8){
-                sum = sum+1;
-            }else if(i<16){
-                sum= sum +2;
-            }else if(i<24){
-                sum = sum+3;
-            }else{
-                sum = sum+4;
-            }
+        for(int l=0;l<word.length();l++){
+            if(l<8){
+          sum += 1;
+        }else if(l<16){
+            sum += 2;
+        }else if(l<24){
+            sum += 3;
+        }else{
+            sum += 4;
+        } 
         }
+       
         return sum;
-
     }
 }
